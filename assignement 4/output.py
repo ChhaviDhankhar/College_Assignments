79751c403d6d588c530aa0318da03053aad01540
ChhaviDhankhar <104223694+ChhaviDhankhar@users.noreply.github.com>
2022-06-07 14:49:01 +0530

    Create output.py

---
+++ assignement 4/output.py
@@ -0,0 +1,43 @@
+#QUES 1
+
+enter marks =34
+grade is E
+
+#QUES 2
+enter year= 2014
+2014 is not a leap year
+
+#QUES 3
+Question  1 : 4 x 9 =
+45
+Wrong. The answer is 36
+Question  2 : 1 x 1 =
+1
+Right!
+Question  3 : 6 x 7 =
+34
+Wrong. The answer is 42
+Question  4 : 5 x 8 =
+2
+Wrong. The answer is 40
+Question  5 : 8 x 5 =
+40
+Right!
+Question  6 : 7 x 4 =
+34
+Wrong. The answer is 28
+Question  7 : 1 x 9 =
+9
+Right!
+Question  8 : 8 x 7 =
+56
+Right!
+Question  9 : 7 x 7 =
+23
+Wrong. The answer is 49
+Question  10 : 0 x 8 =
+0
+Right!
+
+#QUES 4
+177 candies are in the bowl!
